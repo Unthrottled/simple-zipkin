e8a6e3a6fe38e0fe894d449c4dca08351985cc93
cyclic-reference <alex@cyclicreference.space>
2017-05-20 20:33:15 -0500

    More Zipkin Server, More Using Zipkin, More Streams, More Rabbitmq

--- zipkin-server/src/main/java/io/acari/zipkinserver/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/io/acari/zipkinserver/ZipkinServerApplication.java
@@ -2,11 +2,13 @@ package io.acari.zipkinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
+@EnableZipkinStreamServer
 @SpringBootApplication
 public class ZipkinServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZipkinServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
 }
